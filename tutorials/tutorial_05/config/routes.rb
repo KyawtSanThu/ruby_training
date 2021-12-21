3ff661d3aa7df89d119ee1f3c1ff109322ab355b
KyawtSanThu <jue23298@gmail.com>
2021-12-22 00:33:05 +0630

    [master] finish tutorial_05

---
+++ tutorials/tutorial_05/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/file',to: 'tutorial5#file'
+  post "/file/read",to: "tutorial5#read_data" 
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
