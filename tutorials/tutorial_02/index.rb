abc1b01d9b566b87973b5d92fed7a4ca55752dba
KyawtSanThu <jue23298@gmail.com>
2021-12-28 13:38:16 +0630

    [master]fixed tutorial2

--- tutorials/tutorial_02/index.rb
+++ tutorials/tutorial_02/index.rb
@@ -4,7 +4,6 @@ width = height * 2 - 1
 for row in 0..height
   for column in 0..width + 1
     if column >= height - row && column <= height + row
-
       print "*"
     elsif print " "
     end
@@ -16,7 +15,6 @@ end
 for row in (height - 1).downto(0)
   for column in (width + 1).downto(0)
     if column >= height - row && column <= height + row
-      
       print "*"
     elsif print " "
     end
