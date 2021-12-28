96ffae8d0190aac495d06a08ec5cfdabcd60e244
KyawtSanThu <jue23298@gmail.com>
2021-12-28 13:35:58 +0630

    [master]fixed tutorial1

--- tutorials/tutorial_01/index.rb
+++ tutorials/tutorial_01/index.rb
@@ -4,7 +4,6 @@ row_range = 1..8
 for row in row_range
   for column in column_range
     if row == 1 || row == 3 || row == 5 || row == 7
-      
       print " 0  1 "
     else
       print " 1  0 "
